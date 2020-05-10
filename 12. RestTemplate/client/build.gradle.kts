5db268ea5a4276f702f2b989cd5bf7f0ca3b1c1b
jayden-lee <gwanggi.lee@gmail.com>
2020-05-11 01:06:26 +0900

    Add httpclient dependency

--- 12. RestTemplate/client/build.gradle.kts
+++ 12. RestTemplate/client/build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.apache.httpcomponents:httpclient")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
