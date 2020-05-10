5282c201806d01e0a3dcd17722b469906c3a0bd6
jayden-lee <gwanggi.lee@gmail.com>
2020-05-11 01:06:34 +0900

    Add log interface

---
+++ 12. RestTemplate/client/src/main/kotlin/com/jayden/resttemplate/client/common/log/Log.kt
@@ -0,0 +1,8 @@
+package com.jayden.resttemplate.client.common.log
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+interface Log {
+    val logger: Logger get() = LoggerFactory.getLogger(this.javaClass)
+}
